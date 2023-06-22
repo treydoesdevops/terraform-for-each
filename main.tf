e264389ec1dd26027555d972de9657277ab46b8b
Trey Devops <treydoesdevops@gmail.com>
2023-06-22 19:45:49 -0400

    Resource Group For Each Example

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
-#tf
+provider "azurerm" {
+  features {}
+}
+
+locals {
+    resource_group_names = {
+        rg1 = "eastus",
+        rg2 = "eastus2",
+        rg3 = "westus"
+    }
+}
+
+resource "azurerm_resource_group" "rg" {
+  for_each = local.resource_group_names
+  name     = each.key
+  location = each.value
+}
